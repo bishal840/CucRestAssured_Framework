eb9c32ccdc0be9724b7bb24c27ead11e75bbcfc5
bishal840 <bishalmishra84@gmail.com>
2023-03-28 13:26:58 +0530

    in TestRunner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -2,6 +2,7 @@ package Runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeClass;
 
 @CucumberOptions(
 		features="src/test/java/features",monochrome=true,
@@ -11,10 +12,11 @@ import io.cucumber.testng.CucumberOptions;
 		plugin={"pretty","html:target/cucumber.html","json:target/jsonReports/cucumber-report.json"})
 //
 public class TestRunner extends AbstractTestNGCucumberTests {
-//		@BeforeClass
-//		public void before1()
-//		{
-//			System.out.println("TestNg");
-//		}
+		@BeforeClass
+		public void before1()
+		{
+			System.out.println("Print TestNg");
+		}
+		///there is change here
 }
 	
